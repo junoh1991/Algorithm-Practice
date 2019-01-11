b9cc46e8dfda439e97c583b7a9d6a23b7947625b
Marcus <ohxxx371@umn.edu>
2019-01-11 17:59:52 -0600

    bfs

--- dataStructure/graph.cc
+++ dataStructure/graph.cc
@@ -17,8 +17,9 @@ public:
 
 	void printGraph();
 	
-	void traverseBFS(int start);
+	int *traverseBFS(int start);
 	
+	void printPath(int start, int end, int *parent);
 
 private:
 	vector<int> *adj_;
@@ -52,12 +53,14 @@ void Graph::printGraph(){
 
 }
 
-void Graph::traverseBFS(int start){
+int *Graph::traverseBFS(int start){
 	queue <int> q;
+	int *parent = new int[num_vertex_];
 	bool color[num_vertex_];
 	for (int i = 0; i < num_vertex_; i++){
 		color[i] = WHITE;
 	}
+	parent[start] = -1;
 	q.push(start);
 	
 	while(!q.empty()){
@@ -66,25 +69,37 @@ void Graph::traverseBFS(int start){
 			if (color[adj_[front][i]] == WHITE){
 				q.push(adj_[front][i]);
 				color[adj_[front][i]] = GRAY;
+				parent[adj_[front][i]] = front;
 			}
 		}
 		q.pop();
 		color[front] = BLACK;
 	}
-	
+
+	return parent;
 }
+
+void Graph::printPath(int start, int end, int *parent){
+	if (start == end || end == -1)
+		printf("\n%d", start);
+	else{
+		printPath(start, parent[end], parent);
+		printf(" %d", end);
+	}
+}
+
+
 
 // Test Program
 int main(){
 
-	Graph example(5);
-	example.insertEdge(0,1);
-	example.insertEdge(0,4);
+	Graph example(7);
 	example.insertEdge(1,2);
-	example.insertEdge(1,3);
-	example.insertEdge(1,4);
+	example.insertEdge(1,5);
+	example.insertEdge(1,6);
+	example.insertEdge(2,5);
 	example.insertEdge(2,3);
 	example.insertEdge(3,4);
-
+	example.insertEdge(4,5);
 	
 }
